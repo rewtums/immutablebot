bdfb358313027fa317c5598f8b50375bc195ad41
rewt <rewt@mailbox.org>
2015-12-11 08:04:35 -0500

    Move configuration from individual functions to module attributes

--- lib/immutablebot/server.ex
+++ lib/immutablebot/server.ex
@@ -1,58 +1,59 @@
 defmodule Immutablebot.Server do
   use GenServer
 
+  @name __MODULE__
+  @nick Application.get_env(:immutablebot, :nick)
+  @channel Application.get_env(:immutablebot, :channel)
+  @server Application.get_env(:immutablebot, :server)
+  @port Application.get_env(:immutablebot, :port )
+
   #####
   # External API
 
   def start_link do
-    { :ok, pid } = GenServer.start_link(__MODULE__, [], name: __MODULE__)
+    { :ok, pid } = GenServer.start_link(@name, [], name: @name)
   end
 
   def connect do
-    GenServer.call __MODULE__, :connect
+    GenServer.call @name, :connect
   end
 
   def say(phrase, target) do
     data = "PRIVMSG #{target} :#{phrase}"
-    GenServer.cast __MODULE__, { :send, data }
+    GenServer.cast @name, { :send, data }
   end
 
   def join(channel) do
     data = "JOIN #{ channel }"
-    GenServer.cast __MODULE__, { :send, data }
+    GenServer.cast @name, { :send, data }
   end
 
   def set_nick(nickname) do
     data = "NICK #{nickname}"
-    GenServer.cast __MODULE__, { :send, data }
+    GenServer.cast @name, { :send, data }
   end
 
   #####
   # GenServer implementation
 
   def handle_call(:connect, _from, _) do
-    server = Application.get_env(:immutablebot, :server)
-    port = Application.get_env(:immutablebot, :port )
-    nick = Application.get_env(:immutablebot, :nick)
-    { :ok, socket } = :ssl.connect(:erlang.binary_to_list(server), port, [:binary, {:active, true}])
-    GenServer.cast __MODULE__, { :send, "NICK #{nick}"}
-    GenServer.cast __MODULE__, { :send, "USER #{nick} #{server} #{nick} :#{nick}" }
+    { :ok, socket } = :ssl.connect(:erlang.binary_to_list(@server), @port, [:binary, {:active, true}])
+    GenServer.cast @name, { :send, "NICK #{@nick}"}
+    GenServer.cast @name, { :send, "USER #{@nick} #{@server} #{@nick} :#{@nick}" }
     { :reply, socket, socket }
   end
 
   def handle_cast({:new_line, data}, socket) do
-    nick = Application.get_env(:immutablebot, :nick)
-    channel = Application.get_env(:immutablebot, :channel)
-    server = Enum.at(Regex.split(~r/\s/, data), 1)
+    connected_server = Enum.at(Regex.split(~r/\s/, data), 1)
     ping      = ~r/\APING/
     motd_end  = ~r/\/MOTD/
     privmsg   = ~r/PRIVMSG/
-    { :ok, private_message } = Regex.compile("PRIVMSG #{nick} ")
-    { :ok, channel_message }  = Regex.compile("PRIVMSG #{channel} ")
+    { :ok, private_message } = Regex.compile("PRIVMSG #{@nick} ")
+    { :ok, channel_message }  = Regex.compile("PRIVMSG #{@channel} ")
     IO.puts data
 
-    if Regex.match?(motd_end, data), do: GenServer.cast __MODULE__, { :send, "JOIN #{channel}" }
-    if Regex.match?(ping, data), do: GenServer.cast __MODULE__, { :send, "PONG #{server}" }
+    if Regex.match?(motd_end, data), do: GenServer.cast @name, { :send, "JOIN #{@channel}" }
+    if Regex.match?(ping, data), do: GenServer.cast @name, { :send, "PONG #{connected_server}" }
     if Regex.match?(privmsg, data) do
       message = String.split(data, ":")
       phrase = String.strip(Enum.at message, Enum.count(message)-1)
@@ -69,10 +70,10 @@ defmodule Immutablebot.Server do
           target = speaker_name
         end
         if Regex.match?(channel_message, data) do
-          target = channel
+          target = @channel
         end
 
-        GenServer.cast __MODULE__, { :send, "PRIVMSG #{target} :#{result}" }
+        GenServer.cast @name, { :send, "PRIVMSG #{target} :#{result}" }
       end
     end
     { :noreply, socket }
@@ -85,7 +86,7 @@ defmodule Immutablebot.Server do
   end
 
   def handle_info({_, _, data}, socket) do
-    GenServer.cast __MODULE__, { :new_line, data }
+    GenServer.cast @name, { :new_line, data }
     { :noreply, socket }
   end
 end
