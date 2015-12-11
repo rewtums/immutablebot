2c9843b1b5ba696ea4f75815d916956dc6562212
rewt <rewt@mailbox.org>
2015-12-11 15:49:54 -0500

    Refactor logic for identifying an incoming message to use Regex.split/3 and String.split/2 to assign the message parts.
    Use Logger as opposed to IO.puts/1.

--- lib/immutablebot/Commands/agent.ex
+++ lib/immutablebot/Commands/agent.ex
@@ -2,16 +2,16 @@ defmodule Command.Agent do
     @name { :global, __MODULE__ }
 
     def start_link do
-        Agent.start_link( fn-> HashSet.new end, name: @name )
-    end 
+      Agent.start_link( fn-> HashSet.new end, name: @name )
+    end
 
     def add(command) do
-        Agent.update(@name, fn(set) -> Set.put(set, command) end)
+      Agent.update(@name, fn(set) -> Set.put(set, command) end)
     end
 
     def find(phrase) do
-        set = Agent.get(@name, fn(set) -> set end)
-        Enum.find(set, fn ({ pattern, _ }) -> Regex.match?(pattern, phrase) end) 
+      set = Agent.get(@name, fn(set) -> set end)
+      Enum.find(set, fn ({ pattern, _ }) -> Regex.match?(pattern, phrase) end)
     end
 
     def load(phrase, func) do
--- lib/immutablebot/server.ex
+++ lib/immutablebot/server.ex
@@ -1,5 +1,6 @@
 defmodule Immutablebot.Server do
   use GenServer
+  require Logger
 
   @name __MODULE__
   @nick Application.get_env(:immutablebot, :nick)
@@ -44,43 +45,42 @@ defmodule Immutablebot.Server do
   end
 
   def handle_cast({:new_line, data}, socket) do
+
     connected_server = Enum.at(Regex.split(~r/\s/, data), 1)
     ping      = ~r/\APING/
     motd_end  = ~r/\/MOTD/
-    privmsg   = ~r/PRIVMSG/
-    { :ok, private_message } = Regex.compile("PRIVMSG #{@nick} ")
-    { :ok, channel_message }  = Regex.compile("PRIVMSG #{@channel} ")
-    IO.puts data
+    privmsg   = ~r/ PRIVMSG /
+
+    Logger.debug data
 
     if Regex.match?(motd_end, data), do: GenServer.cast @name, { :send, "JOIN #{@channel}" }
     if Regex.match?(ping, data), do: GenServer.cast @name, { :send, "PONG #{connected_server}" }
     if Regex.match?(privmsg, data) do
-      message = String.split(data, ":")
-      phrase = String.strip(Enum.at message, Enum.count(message)-1)
+      [ info, phrase ] = Enum.map(Regex.split(~r/:/, data, [trim: true, parts: 2]), &(String.strip(&1)))
+      [ user, target ] = Enum.map(String.split(info, "PRIVMSG"), &(String.strip(&1)))
+      [ speaker_name, username ] = Enum.map(String.split(user, "!"), &(String.strip(&1)))
+
       command = Command.Agent.find(phrase)
 
       if command do
         { pattern, func } = command
-        args = Regex.scan(pattern, phrase, capture: :all_but_first)
-        speaker_name = Enum.at(String.split(Enum.at(message, 1), "!"), 0)
-        args = Enum.filter(args, &((Enum.count &1) > 0))
-        result = func.(speaker_name, Enum.at(args, 0))
+        args = Regex.split(pattern, phrase, parts: 2)
+        result = func.(speaker_name, Enum.at(args, 1))
 
-        if Regex.match?(private_message, data) do
-          target = speaker_name
-        end
-        if Regex.match?(channel_message, data) do
-          target = @channel
+        if target == "#{@nick}" do
+          reply_target = speaker_name
+        else
+          reply_target = target
         end
 
-        GenServer.cast @name, { :send, "PRIVMSG #{target} :#{result}" }
+        GenServer.cast @name, { :send, "PRIVMSG #{reply_target} :#{result}" }
       end
     end
     { :noreply, socket }
   end
 
   def handle_cast({:send, data}, socket) do
-    IO.puts "sending #{data}"
+    Logger.debug "sending #{data}"
     :ssl.send(socket, "#{data} \r\n")
     { :noreply, socket }
   end
